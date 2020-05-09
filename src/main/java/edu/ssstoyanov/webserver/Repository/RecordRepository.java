1fa36fb2f7296c1ca12cc13630b368125c08bf50
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-10 04:41:46 +0500

    add RecordRepository

--- src/main/java/edu/ssstoyanov/webserver/Repository/RecordRepository.java
+++ src/main/java/edu/ssstoyanov/webserver/Repository/RecordRepository.java
@@ -1,7 +1,22 @@
 package edu.ssstoyanov.webserver.Repository;
 
 import edu.ssstoyanov.webserver.Model.Record;
+import edu.ssstoyanov.webserver.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+/**
+ * @author Stanislav Stoianov
+ * @version 1.3
+ * @apiNote Hibernate class used to record points in the players table
+ * @see Record
+ * @see edu.ssstoyanov.webserver.Service.RecordService
+ * @since 16/04/20
+ */
+
+@Repository
 public interface RecordRepository extends JpaRepository<Record, Integer> {
+    List<Record> getAllByUser(User user);
 }

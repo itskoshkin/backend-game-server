27bd93a31e4598fb82de76bf43ed891a1d6d7f64
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-10 03:29:15 +0500

    added @Entity User.java to work with Spring Security

--- src/main/java/edu/ssstoyanov/webserver/Model/User.java
+++ src/main/java/edu/ssstoyanov/webserver/Model/User.java
@@ -1,67 +1,50 @@
 package edu.ssstoyanov.webserver.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Stanislav Stoianov
- * creation date: 17/04/20.
- * @since 1.0
+ * @version 1.4
+ * @apiNote the Hibernate class used to store user data is controlled using Spring Security
+ * @see edu.ssstoyanov.webserver.Service.UserService
+ * @see edu.ssstoyanov.webserver.Repository.UserRepository
+ * @see edu.ssstoyanov.webserver.Service.UserHandlingService
+ * @since 10/04/20
  */
 
-// TODO: 20.04.2020  
-
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Table(name = "users",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = "login")
-        })
+@Table(name = "users")
 public class User {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "userid", unique = true, nullable = false)
-    private int id;
-
-    @Column(name = "login", unique = true, nullable = false, length = 32)
-    private String login;
-
-    @Column(name = "password", nullable = false, length = 32)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Integer id;
+    @Column(name = "user_name")
+    @Length(min = 5, max = 32, message = "*Username should be at least 5 and no more than 32 characters")
+    @NotEmpty(message = "*Please provide a user name")
+    private String username;
+    @Column(name = "password")
+    @Length(min = 5, message = "*Password should be at least 5 characters")
+    @NotEmpty(message = "*Please provide your password")
     private String password;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Record> records;
-
-    public User() {
-    }
-
-    public User(String login, String password) {
-        this.login = login;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String name) {
-        this.login = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private List<Record> record;
 }

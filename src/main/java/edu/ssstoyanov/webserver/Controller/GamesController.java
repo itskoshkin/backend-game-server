8f5774e6b919e70ac4adc4c712325bb63d272e56
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-10 04:38:08 +0500

    add GamesController

---
+++ src/main/java/edu/ssstoyanov/webserver/Controller/GamesController.java
@@ -0,0 +1,23 @@
+package edu.ssstoyanov.webserver.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Stanislav Stoianov
+ * @since 18/04/20
+ * @version 1.0
+ */
+
+
+@Controller
+public class GamesController {
+
+    @GetMapping(value = "/games")
+    public ModelAndView main() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("games");
+        return modelAndView;
+    }
+}

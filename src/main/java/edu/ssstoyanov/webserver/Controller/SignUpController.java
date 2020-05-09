3d6709123973284b78c53d6644e9242956622e20
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-10 04:36:23 +0500

    add SignInController

---
+++ src/main/java/edu/ssstoyanov/webserver/Controller/SignUpController.java
@@ -0,0 +1,57 @@
+package edu.ssstoyanov.webserver.Controller;
+
+import edu.ssstoyanov.webserver.Model.User;
+import edu.ssstoyanov.webserver.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+/**
+ * @author Stanislav Stoianov
+ * @since 14/04/20
+ * @version 1.7
+ */
+
+@Controller
+public class SignUpController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping(value = "/signup")
+    public ModelAndView signup() {
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("signup");
+        return modelAndView;
+    }
+
+    @PostMapping(value = "/signup")
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByUserName(user.getUsername());
+
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("username", "error.user",
+                            "There is already a user registered with the user name provided");
+        }
+
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("signup");
+        } else {
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("signup");
+
+        }
+        return modelAndView;
+    }
+}

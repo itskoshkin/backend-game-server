4bd6059008a4a7c89af814cae7975b63025a8795
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-10 04:53:41 +0500

    Draft commit

--- src/main/java/edu/ssstoyanov/webserver/Application.java
+++ src/main/java/edu/ssstoyanov/webserver/Application.java
@@ -3,6 +3,12 @@ package edu.ssstoyanov.webserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Stanislav Stoianov
+ * @version 1.0
+ * @since 05/04/20
+ */
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {

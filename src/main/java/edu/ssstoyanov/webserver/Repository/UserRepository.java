6eb8e3f51df36f5d6ee2702f48544d9404ab3f66
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-10 04:43:56 +0500

    added UserRepository

--- src/main/java/edu/ssstoyanov/webserver/Repository/UserRepository.java
+++ src/main/java/edu/ssstoyanov/webserver/Repository/UserRepository.java
@@ -2,9 +2,19 @@ package edu.ssstoyanov.webserver.Repository;
 
 import edu.ssstoyanov.webserver.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+/**
+ * @author Stanislav Stoianov
+ * @version 1.4
+ * @apiNote the Hibernate class used to store user data is controlled using Spring Security
+ * @see edu.ssstoyanov.webserver.Service.UserService
+ * @see User
+ * @see edu.ssstoyanov.webserver.Service.UserHandlingService
+ * @since 10/04/20
+ */
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    boolean findByLogin(String login);
-
-    boolean findByLoginAndPassword(String login, String password);
+    User findByUsername(String username);
 }

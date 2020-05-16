6f2f571a0b364590a73e20e2a2ad2e0d7e37fbe1
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-16 13:34:37 +0500

    add @Transactional method saveRecordFromGamesPost();

--- src/main/java/edu/ssstoyanov/webserver/Service/RecordService.java
+++ src/main/java/edu/ssstoyanov/webserver/Service/RecordService.java
@@ -4,14 +4,16 @@ import edu.ssstoyanov.webserver.Model.Record;
 import edu.ssstoyanov.webserver.Model.User;
 import edu.ssstoyanov.webserver.Repository.RecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
 
 /**
  * @author Stanislav Stoianov
- * @version 1.3
+ * @version 1.5
  * @see Record
  * @see edu.ssstoyanov.webserver.Repository.RecordRepository
  * @since 16/04/20
@@ -23,6 +25,9 @@ public class RecordService {
     @Autowired
     private RecordRepository recordRepository;
 
+    @Autowired
+    private UserService userService;
+
     public HashMap<String, Long> getRecordsByUser(User user) {
         HashMap<String, Long> data = new HashMap<>();
         List<Record> recordList = recordRepository.getAllByUser(user);
@@ -32,11 +37,21 @@ public class RecordService {
         return data;
     }
 
-    public Record saveRecord(String game, Long score, User user){
+    @Transactional
+    public Record saveRecord(String game, Long score, User user) {
         Record record = new Record();
         record.setUser(user);
         record.setScore(score);
         record.setGame(game);
+        recordRepository.saveAndFlush(record);
+        return record;
+    }
+
+    @Transactional
+    public Record saveRecordFromGamesPost(Record record) {
+        User user = userService.findUserByUserName(SecurityContextHolder.getContext().getAuthentication().getName());
+        record.setUser(user);
+        record.setId(null);
         recordRepository.saveAndFlush(record);
         return record;
     }

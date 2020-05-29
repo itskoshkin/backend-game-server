4118491db14c623b8d4ccf99ca87caa2ea3fea9f
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-29 20:38:42 +0300

    add new methods to work with new REST API

--- src/main/java/edu/ssstoyanov/webserver/Service/RecordService.java
+++ src/main/java/edu/ssstoyanov/webserver/Service/RecordService.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 /**
  * @author Stanislav Stoianov
- * @version 1.5
+ * @version 1.6
  * @see Record
  * @see edu.ssstoyanov.webserver.Repository.RecordRepository
  * @since 16/04/20
@@ -28,6 +28,20 @@ public class RecordService {
     @Autowired
     private UserService userService;
 
+    @Transactional
+    public void saveRecordFromGamesPost(Record record) {
+        User user = userService.findUserByUserName(SecurityContextHolder.getContext().getAuthentication().getName());
+        record.setUser(user);
+        record.setId(null);
+        recordRepository.saveAndFlush(record);
+    }
+
+    @Transactional
+    public Record saveRecordFromPost(Record record) {
+        return recordRepository.saveAndFlush(record);
+    }
+
+
     public HashMap<String, Long> getRecordsByUser(User user) {
         HashMap<String, Long> data = new HashMap<>();
         List<Record> recordList = recordRepository.getAllByUser(user);
@@ -37,13 +51,12 @@ public class RecordService {
         return data;
     }
 
-    @Transactional
-    public Record saveRecordFromGamesPost(Record record) {
-        User user = userService.findUserByUserName(SecurityContextHolder.getContext().getAuthentication().getName());
-        record.setUser(user);
-        record.setId(null);
-        recordRepository.saveAndFlush(record);
-        return record;
+    public List<Record> getAllRecords() {
+        return recordRepository.findAll();
+    }
+
+    public List<Record> getAllRecordsByUsername(String username) {
+        return recordRepository.getByUser(userService.findUserByUserName(username));
     }
 
 }
